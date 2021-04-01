20cba070a6340cf24bbc0d52501f8b4a6d8304f5
Mariana <mritchie@Marianas-MacBook-Air.local>
2021-04-01 18:27:35 -0400

    Added flag for custom paramaterization

--- model/build_model.py
+++ model/build_model.py
@@ -6,11 +6,13 @@ from matplotlib import pyplot as plt
 parser = argparse.ArgumentParser(description="Takes in file name.")
 
 parser.add_argument('-f', '--file', required=True, help="Path to image file.")
+parser.add_argument('-p', '--parameters', nargs=2, help="Specifications for edge detection analysis", type=int, default=(100,200))
 
 args = parser.parse_args()
+p1, p2 = args.parameters
 
 img = cv.imread(args.file, 0)
-edges = cv.Canny(img,100,200)
+edges = cv.Canny(img,p1,p2)
 
 plt.subplot(121)
 plt.imshow(img,cmap = 'gray')
